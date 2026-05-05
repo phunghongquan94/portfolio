62b78a86e42ff14381638867b87735b1937dffaa
Phùng Hồng Quân <135598758+phunghongquan94@users.noreply.github.com>
2026-05-05 22:31:59 +0700

    Delete js directory

--- js/cursor.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.pageY + "px";
-  cursor.style.left = e.pageX + "px";
-  cursor.style.display = "block";
-});
